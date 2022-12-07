c4bfe9ee75f2488170795be0597430b746f963aa
uo282716 <113901140+uo282716@users.noreply.github.com>
2022-12-07 08:08:57 +0100

    Delete PRACTICA-3 directory

--- PRACTICA-3/Ejercicio-02/MasInfoNavegador.js
+++
@@ -1,5 +0,0 @@
-document.write(infoNavegador.version);
-document.write(infoNavegador.plataforma);
-document.write(infoNavegador.vendedor);
-document.write(infoNavegador.agente);
-document.write(infoNavegador.javaActivo);
